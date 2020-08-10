ccbea35f2620d3f76d62b92ad93827a4d5dd1e82
aish2212 <aish.anandh95@gmail.com>
2020-08-10 10:01:08 +0530

    1st commit

--- session4/session4.py
+++ session4/session4.py
@@ -1,121 +1,82 @@
-import fractions
-from fractions import Fraction
+import random
+import cmath
+import decimal
+from decimal import Decimal , ROUND_HALF_UP, localcontext
 
-
-def encoded_from_base10(number, base, digit_map):
+class Qualean:
     '''
-    This function returns a string encoding in the "base" for the the "number" using the "digit_map"
-    Conditions that this function must satisfy:
-    - 2 <= base <= 36 else raise ValueError
-    - invalid base ValueError must have relevant information
-    - digit_map must have sufficient length to represent the base
-    - must return proper encoding for all base ranges between 2 to 36 (including)
-    - must return proper encoding for all negative "numbers" (hint: this is equal to encoding for +ve number, but with - sign added)
-    - the digit_map must not have any repeated character, else ValueError
-    - the repeating character ValueError message must be relevant
-    - you cannot use any in-built functions in the MATH module
+        This is a Qualean Class. 
     '''
-    if base < 2 or base > 36:
-        raise ValueError(" base is out of ranges, It should be between 2 to 36 (including)")
-    elif len(digit_map) > len(set(digit_map)):
-        raise ValueError("digit_map must not have any repeating character")
-    elif len(digit_map) != base:
-        raise ValueError("Length of digit map is not equal to base ")
-    elif number == 0:
-        return 0
-    else:
-        digits = []
-        n,encoding = (-number,"-") if number < 0 else (number,"")
-        while n > 0:
-            m = n % base
-            n = n // base
-            digits.insert(0,m)
-        encoding += "".join([digit_map[d] for d in digits])
-        return encoding
-
-
-def rel_tol(X,Y):  #Function to calculate the relative tolerance
-    try:
-        rl = abs(X - Y) /min(abs(X), abs(Y))
-        if(rl <= 1e-12):
-            return True
+    def __init__(self, user_input):
+        self.user_input = user_input
+        if ((user_input == -1) | (user_input == 0) | (user_input == 1)):
+            with localcontext() as ctx:
+                ctx.rounding = ROUND_HALF_UP
+                ctx.prec = 10
+                self.input_value = Decimal(user_input) * Decimal(random.uniform(-1, 1))
         else:
-            return False
-    except:
-        return False
+            raise ValueError("input value must be either 1 or 0 or -1")
+   
 
+    def __round__(self):
+        return self.input_value
 
-def abs_tol(X,Y): #Function to calculate the absolute tolerance
-    al = abs(X - Y)
-    if(al <= 1e-05):
-        return True
-    else:
-        return False
+    def __and__(self, other):
+        return bool(self.input_value) and bool(other.input_value)
+    
+    def __or__(self, other):
+        return bool(self.input_value) or bool(other.input_value)
+        
+    def __ge__(self, other):
+        return self.input_value >= other.input_value
 
+        
+    def __gt__(self, other):
+        return  self.input_value > other.input_value
 
-def float_equality_testing(a, b):
-    '''
-        This function emulates the ISCLOSE method from the MATH module, but you can't use this function
-        We are going to assume:
-        - rel_tol = 1e-12
-        - abs_tol = 1e-05
-    '''
-    output = (rel_tol(a,b) or abs_tol(a,b))
-    if (output == True):
-        a=b
-    else:
-        a!=b
-    return a==b
+        
+    def __invertsign__(self):
+        return  self.input_value*-1
 
+        
+    def __le__(self, other):
+        return self.input_value <= other.input_value
 
-def manual_truncation_function(f_num):
-    '''
-    This function emulates python's MATH.TRUNC method. It ignores everything after the decimal point. 
-    It must check whether f_num is of correct type before proceed. You can use inbuilt constructors like int, float, etc
-    '''
-    a = 0
-    b = []
-    if(type(f_num) is float):
-        a = f_num
-        for i in str(a):
-            if(i == '.'):
-                break
-            b.append(i)
-        s_num = "".join(b)
-        f_num=Fraction(s_num)
-        return f_num.numerator
+        
+    def __lt__(self, other):
+        return self.input_value < other.input_value
 
+        
+    def __mul__(self, other):
+        with localcontext() as ctx:
+            ctx.rounding = ROUND_HALF_UP
+            ctx.prec = 10
+            return self.input_value * other.input_value
 
-def manual_rounding_function(f_num):
-    '''
-    This function emulates python's inbuild ROUND function. You are not allowed to use ROUND function, but
-    expected to write your one manually.
-    '''
-    a = 0
-    b = []
-    sign = -1 if f_num < 0 else 1
-    if(type(f_num) is float):
-        a = f_num
-        for i in str(a):
-            if(i == '.'):
-                break
-            b.append(i)
-        s_num = "".join(b)
-        f = Fraction(s_num)
-        f_num_round = f.numerator
-        f_num_dec = f_num - f_num_round  
-        if (f_num_dec >= 0.5) and (sign == 1):
-            return f_num_round + 1
-        elif (f_num_dec <= -0.5) and (sign == -1):
-            return f_num_round - 1
-        else:
-            return f_num_round
+            
+    def __eq__(self, other):
+        return self.input_value == other.input_value
 
+        
+    def __add__(self, other):
+        return  self.input_value + other.input_value
 
-def rounding_away_from_zero(f_num):
-    '''
-    This function implements rounding away from zero as covered in the class
-    Desperately need to use INT constructor? Well you can't. 
-    Hint: use FRACTIONS and extract numerator. 
-    '''
-    return 3.0
+        
+    def __str__(self):
+        return 'Qualean_value[{0}] = {1}'.format(self.user_input, self.input_value)
+    
+    
+    def __repr__(self):
+        return 'Qualean_value[{0}] = {1}'.format(self.user_input, self.input_value)
+    
+    
+    def __bool__(self):
+        return bool(self.input_value)
+
+        
+    def __float__(self):
+        return float(self.input_value)
+
+        
+    def __sqrt__(self):
+        return cmath.sqrt(self.input_value)
--- session4/test_session4.py
+++ session4/test_session4.py
@@ -7,48 +7,68 @@ import inspect
 import re
 import math
 
+import decimal
+from decimal import Decimal
+
+ZERO_INPUT_VALUE = 0
+INPUT_VALUE_1 = Decimal(random.choice([-1, 1, 0]))
+INPUT_VALUE_2 = Decimal(random.choice([-1, 1, 0]))
+
 README_CONTENT_CHECK_FOR = [
-    'int',
-    'encoded_from_base10',
-    'digit_map',
-    'ValueError',
-    'math',
-    'isclose',
-    'absolute',
-    'relative',
-    'tolerance',
-    'bin(',
-    'hex(',
-    'round',
-    'truncation',
-    'error',
-    'equality',
-    'zero',
-    'away'
+    'Qualean'
+    '__and__',
+    '__or__',
+    '__repr__',
+    '__str__'
+    '__add__'
+    '__eq__'
+    '__float__'
+    '__ge__'
+    '__gt__'
+    '__invertsign__'
+    '__le__'
+    '__lt__'
+    '__mul__'
+    '__sqrt__'
+    '__bool__'
 ]
 
-CHECK_FOR_THINGS_NOT_ALLOWED = [
-    'math',
-    'isclose',
-    'bin(',
-    'hex(',
-    'round(',
-    'int(',
-    '10.4',
-    '-10.4'
-    '1.25',
-    '-1.25',
+FUNCTION_LIST = [
+    '__and__',
+    '__or__',
+    '__repr__',
+    '__str__'
+    '__add__'
+    '__eq__'
+    '__float__'
+    '__ge__'
+    '__gt__'
+    '__invertsign__'
+    '__le__'
+    '__lt__'
+    '__mul__'
+    '__sqrt__'
+    '__bool__'
 ]
-
+#1
 def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
+#2
 def test_readme_contents():
     readme = open("README.md", "r")
     readme_words = readme.read().split()
     readme.close()
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
+#3
+def test_readme_file_for_formatting():
+    f = open("README.md", "r")
+    content = f.read()
+    f.close()
+    assert content.count("#") >= 10
+
+#4    
 def test_readme_proper_description():
     READMELOOKSGOOD = True
     f = open("README.md", "r")
@@ -60,12 +80,7 @@ def test_readme_proper_description():
             pass
     assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
 
-def test_readme_file_for_formatting():
-    f = open("README.md", "r")
-    content = f.read()
-    f.close()
-    assert content.count("#") >= 10
-
+#5
 def test_indentations():
     ''' Returns pass if used four spaces for each level of syntactically \
     significant indenting.'''
@@ -75,79 +90,132 @@ def test_indentations():
         assert len(space) % 4 == 2, "Your script contains misplaced indentations"
         assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines" 
 
+#6
 def test_function_name_had_cap_letter():
     functions = inspect.getmembers(session4, inspect.isfunction)
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
-def test_invalid_base_valueerror():
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(10, -1, '1234567890')
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(10, 1, '012')
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(10, 37, '1234567890123456789012345678901234567')
+#7
+def test_functions_list():
+    ALL_FUNCTIONS_PRESENT = True
+    functions = inspect.getmembers(session4, inspect.isfunction)
+    for function in functions:
+        if function not in FUNCTION_LIST:
+            assert ALL_FUNCTIONS_PRESENT == False, "You have not added all the functions/class"
+        else:
+            ALL_FUNCTIONS_PRESENT == True
 
-def test_invalid_base_valueerror_provides_relevant_message():
-    with pytest.raises(ValueError, match=r".* base .*"):
-        session4.encoded_from_base10(10, -1, '1234567890')
+#8            
+def test_Qualean_equality():
+    r1 = session4.Qualean(ZERO_INPUT_VALUE)
+    r2 = session4.Qualean(ZERO_INPUT_VALUE)
+    assert r1 == r2, "Both the qualean values are equal"
 
-def test_innacurate_digit_map_length():
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(123123, 16, '0123456789abcde')
+#9   
+def test_Qualean_add():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1+q2)==(round(q1)+round(q2)))
+ 
+#10
+def test_Qualean_mul():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1*q2)==(round(q1)*round(q2)))
 
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(123123, 9, '01234567')
+#11    
+def test_Qualean_and():
+    q1=session4.Qualean(0)
+    q2=session4.Qualean(1)
+    q3=session4.Qualean(-1)
+    assert((q1 & q2)==False)
+    assert((q2 & q3)==True)
+    assert((q1 & q3)==False)
+    
+
+#12
+def test_Qualean_or():
+    q1=session4.Qualean(1)
+    q2=session4.Qualean(-1)
+    q3=session4.Qualean(0)
+    assert((q1 | q2)==True)
+    assert((q2 | q3)==True)
+    assert((q1 | q3)==True)
+ 
+#13 
+def test_Qualean_float():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    assert(float(q1)==float(round(q1)))
+
+#14
+def test_bool():
+    q1=session4.Qualean(0)
+    q2=session4.Qualean(1)
+    q3=session4.Qualean(-1)
+    assert(bool(q1)==False)
+    assert(bool(q2)==True)
+    assert(bool(q3)==True)
+
+#15
+def test_Qualean_repr() :
+    q1=session4.Qualean(INPUT_VALUE_1)
+    assert(repr(q1)==('Qualean_value['+ str(INPUT_VALUE_1) + '] = '+str(round(q1))))
+
+#16
+def test_Qualean_str() :
+    q1=session4.Qualean(INPUT_VALUE_1)
+    assert(str(q1)==('Qualean_value['+ str(INPUT_VALUE_1) + '] = '+str(round(q1))))
+
+#17
+def test_Qualean_ge():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1>=q2)==(round(q1)>=round(q2)))
+
+#18
+def test_Qualean_gt():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1>q2)==(round(q1)>round(q2)))
+    
+#19
+def test_Qualean_le():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1<=q2)==(round(q1)<=round(q2)))
+    
+#20
+def test_Qualean_lt():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    q2=session4.Qualean(INPUT_VALUE_2)
+    assert((q1<q2)==(round(q1)<round(q2)))
+    
+#21
+def test_Qualean_sqrt():
+    q1=session4.Qualean(INPUT_VALUE_1)
+    assert((session4.Qualean.__sqrt__(q1)) == (Decimal(round(q1)).sqrt()))
+    
+#22
+def test_Qualean_sum_1000times():
+    q2 = q1 =session4.Qualean(INPUT_VALUE_1)
+    for i in range(100):
+        q1+= q1
+    assert q1 == Decimal(100)*(q2)
+#23
+#def test_Qualean_q1andq2_q1False_q2notdefined():
+#24
+#def test_Qualean_q1orq2_q1True_q2notdefined():
+#25
+#def test_Qualean_1millionsum_closetozero():
 
 
-def test_hexadecimal_conversions():
-    for _ in range(50):
-        r_num = random.randint(0, 32767)
-        assert (session4.encoded_from_base10(r_num, 16, '0123456789abcdef').lower() ) == hex(r_num)[2:], f"Your program returned wrong HEX conversions"
-
-def test_negative_hexadecimal_conversions():
-    for _ in range(50):
-        r_num = random.randint(-32700, -1)
-        assert (session4.encoded_from_base10(r_num, 16, '0123456789abcdef').lower() ) == '-' + hex(r_num)[3:], f"Your program returned wrong HEX conversions"
 
 
-def test_repeating_digits_in_digit_map():
-    with pytest.raises(ValueError):
-        session4.encoded_from_base10(10, 10, '0123401234')
-
-def test_repeating_digits_valueerror_provides_relevant_message():
-    with pytest.raises(ValueError, match=r".* repeating .*"):
-        session4.encoded_from_base10(10, 10, '012AB012ab'), 'Something is fishy! You are not using word "repeating" while talking about an error releated to "repeating" alphanumerics!!'
-
-def test_float_equality_testing():
-    for _ in range(10000):
-        scale = random.randint(1, 1000000)
-        a = random.uniform(-1.5, 1.6)
-        a, b = a * scale, a * scale - a / scale
-        assert session4.float_equality_testing(a, b) == math.isclose(a, b, rel_tol = 1e-12, abs_tol=1e-05), 'Aap jis number se sampark karna chahte hai, woh is samay uplabdh nahi hai, kripya thodi der baad prayas karein. The numbers you are trying to check right now are not equal, please try again later'
-
-def test_things_not_allowed():
-    code_lines = inspect.getsource(session4)
-    for word in CHECK_FOR_THINGS_NOT_ALLOWED:
-        assert word not in code_lines, 'Have you heard of Pinocchio?'
-
-def test_fraction_used_or_not():
-    code_lines = inspect.getsource(session4)
-    assert 'fractions' in code_lines, 'Fractions not used! You must use fractions'
-    assert 'import' in code_lines, 'You have not imported fractions!'
-
-def test_manual_truncation_function():
-    for _ in range(100):
-        f_num = random.uniform(-100, 100)
-        assert session4.manual_truncation_function(f_num) == math.trunc(f_num), 'Just because you are not able to fix this truncation error, SkyNet is going to rule the earth!'
-
-def test_manual_rounding_function():
-    for f_num in [1.25, 1.35, -1.25, -1.35]:
-        assert session4.manual_rounding_function(f_num) == round(f_num), 'Terminator after looking at your code: I will be back! He will come back when you fix your rounding errors.'
 
 
-def test_functions_for_zero():
-    assert session4.float_equality_testing(0.0, 0.0), 'How can zero be not equal to zero?'
-    assert session4.manual_truncation_function(0.0) == 0, 'Tuncation of 0 should be zero'
-    assert session4.manual_rounding_function(0.0) == 0, 'Zero can only be rounded off to zero'
 
+
+
+
+    
